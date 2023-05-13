35324dca6617bb5dacea02151708d5e6a8ffe80d
Cath1999 <catherine.mena11751@est.uca.edu.ni>
2023-05-13 12:38:45 -0600

    ¨update¨

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PatientComponent } from './entities/patient/patient.component';
+import { EmployeeComponent } from './entities/employee/employee.component';
+import { CountryComponent } from './entities/country/country.component';
+import { DepartmentComponent } from './entities/department/department.component';
+import { AddPatientComponent } from './add/add-patient/add-patient.component';
+import { AddEmployeeComponent } from './add/add-employee/add-employee.component';
+import { AddCountryComponent } from './add/add-country/add-country.component';
+import { AddDepartmentComponent } from './add/add-department/add-department.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    EmployeeComponent,
+    CountryComponent,
+    DepartmentComponent,
+    AddPatientComponent,
+    AddEmployeeComponent,
+    AddCountryComponent,
+    AddDepartmentComponent,
+
   ],
   imports: [
     BrowserModule
